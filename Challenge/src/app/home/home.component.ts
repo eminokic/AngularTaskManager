2d5254711c39a7bd14c69d2991d7dc053ecc5fb4
Emin <eminokic@iastate.edu>
2020-08-05 09:15:46 -0500

    Adding task bar, enter text, and can add / remove task

--- Challenge/src/app/home/home.component.ts
+++ Challenge/src/app/home/home.component.ts
@@ -1,9 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, NgModule, Input } from "@angular/core";
+import { FormsModule } from '@angular/forms'
+
+
 
 @Component({
   selector: "thx-home",
   templateUrl: "./home.component.html",
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent  {
-
+      /* An empty array that is responsible 
+       to add a division */
+       public items = []; 
+  
+       /* A two-way binding performed which 
+          pushes text on division */
+       public Task; 
+     
+     
+       /* When input is empty, it will 
+          not create a new division */
+       public addTask() { 
+           if (this.Task == '') { 
+           } 
+           else { 
+               this.items.push(this.Task); 
+               this.Task = ''; 
+           } 
+       } 
+     
+       /* This function takes to input the 
+          task, that has to be deleted*/
+       public deleteTask(index) { 
+           this.items.splice(index, 1); 
+       } 
 }
